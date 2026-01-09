c0f12eabf51f7e25a382c08056487388781fb3d9
KEERTHANA K H <keerthanakh01@gmail.com>
2026-01-09 19:20:48 +0530

    Create if_else_basics.py

---
+++ Day09_Decision_Making/if_else_basics.py
@@ -0,0 +1,10 @@
+# Day 9 - Decision Making (if-else)
+
+age = int(input("Enter age: "))
+
+if age < 5:
+    print("Free bus ticket")
+elif age >= 60:
+    print("Senior citizen discount")
+else:
+    print("Full ticket price")
