d500d6ff7677a08d3c987cf69e16af1551262943
KEERTHANA K H <keerthanakh01@gmail.com>
2026-01-12 15:19:44 +0530

    Update list_dict_loops.py

--- Day12_List_Dict_ForLoops/list_dict_loops.py
+++ Day12_List_Dict_ForLoops/list_dict_loops.py
@@ -1,1 +1,13 @@
+# Day 12 - Lists & Dictionaries with For Loops
+
+numbers = [10, 20, 30, 40]
+
+total = 0
+for num in numbers:
+    total += num
+print("Total:", total)
+
+student = {"name": "Keerthana", "age": 20}
+for key, value in student.items():
+    print(key, value)
 
