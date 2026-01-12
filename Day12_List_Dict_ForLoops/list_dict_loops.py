d68cbca3e72df7f330a0897e840cc4035ce9b3a5
KEERTHANA K H <keerthanakh01@gmail.com>
2026-01-12 15:20:20 +0530

    Update list_dict_loops.py

--- Day12_List_Dict_ForLoops/list_dict_loops.py
+++ Day12_List_Dict_ForLoops/list_dict_loops.py
@@ -10,4 +10,16 @@ print("Total:", total)
 student = {"name": "Keerthana", "age": 20}
 for key, value in student.items():
     print(key, value)
+# List comprehension
+squares = [x * x for x in range(1, 6)]
+print(squares)
+
+# Dictionary comprehension
+square_dict = {x: x * x for x in range(1, 6)}
+print(square_dict)
+
+# List input using split
+values = input("Enter numbers: ").split()
+values = [int(x) for x in values]
+print(values)
 
