9aa20b797cbb68348dbb17522ca6b75a4b151779
KEERTHANA K H <keerthanakh01@gmail.com>
2026-01-13 20:49:32 +0530

    Update debugging_basics.py

--- Day13_Debugging/debugging_basics.py
+++ Day13_Debugging/debugging_basics.py
@@ -5,4 +5,12 @@ i = 0
 while i <= 5:
     print("Value of i:", i)
     i = i + 1
+# Tracing execution step by step
+
+count = 1
+while count <= 3:
+    print("Loop running, count =", count)
+    count += 1
+
+print("Loop finished")
 
