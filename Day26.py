24d0015d57df41ad24b9d3cb8feb648f36ae3d11
KEERTHANA K H <keerthanakh01@gmail.com>
2026-02-02 19:37:17 +0530

    Add Day 26 notes on Python packages and projects
    Documented learning outcomes related to Python packages and projects.

---
+++ Day26.py
@@ -0,0 +1,11 @@
+## Day 26 – Python Packages & Practical Projects
+
+### What I Learned:
+- How to install and use third-party Python packages using pip
+- Exploring PyPI (Python Package Index)
+- Using external libraries in real projects
+- Generated a QR code using a Python library
+- Created a QR code for my GitHub profile
+- Built a simple currency converter using a Python package
+
+📌 This day helped me understand how Python libraries are used in real-world applications.
