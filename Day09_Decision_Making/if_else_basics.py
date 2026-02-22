f0682c636ebacab235ea205cdb4f4cbcb13a7170
KEERTHANA K H <keerthanakh01@gmail.com>
2026-02-22 18:09:54 +0530

    Update if_else_basics.py

--- Day09_Decision_Making/if_else_basics.py
+++ Day09_Decision_Making/if_else_basics.py
@@ -3,7 +3,6 @@ age = int(input("Enter age: "))
 # Logical operators (and, or)
 if age >= 0 and age < 5:
     print("Free bus ticket")
-
 elif age >= 60 or age == 59:
     print("Senior citizen discount")
 
